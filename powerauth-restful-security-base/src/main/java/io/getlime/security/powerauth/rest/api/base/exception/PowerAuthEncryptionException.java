219bdf20e74362f6a027ea4fda3598d2380ef7e6
Roman Strobl <roman.strobl@lime-company.eu>
2018-06-20 15:15:37 +0200

    Fix #47: Prepare example code for encrypted data exchange

---
+++ powerauth-restful-security-base/src/main/java/io/getlime/security/powerauth/rest/api/base/exception/PowerAuthEncryptionException.java
@@ -0,0 +1,57 @@
+/*
+ * PowerAuth integration libraries for RESTful API applications, examples and
+ * related software components
+ *
+ * Copyright (C) 2017 Lime - HighTech Solutions s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.getlime.security.powerauth.rest.api.base.exception;
+
+/**
+ * Exception raised in case encryption or decryption fails.
+ *
+ * @author Roman Strobl, roman.strobl@lime-company.eu
+ *
+ */
+public class PowerAuthEncryptionException extends Exception {
+
+    private static final long serialVersionUID = -4247463135868495185L;
+
+    private static final String DEFAULT_CODE = "ERR_ENCRYPTION";
+    private static final String DEFAULT_ERROR = "POWER_AUTH_ENCRYPTION_FAILED";
+
+    /**
+     * Default constructor
+     */
+    public PowerAuthEncryptionException() {
+        super(DEFAULT_ERROR);
+    }
+
+    /**
+     * Constructor with a custom error message
+     * @param message Error message
+     */
+    public PowerAuthEncryptionException(String message) {
+        super(message);
+    }
+
+    /**
+     * Get the default error code, used for example in REST response.
+     * @return Default error code.
+     */
+    public String getDefaultCode() {
+        return DEFAULT_CODE;
+    }
+}
