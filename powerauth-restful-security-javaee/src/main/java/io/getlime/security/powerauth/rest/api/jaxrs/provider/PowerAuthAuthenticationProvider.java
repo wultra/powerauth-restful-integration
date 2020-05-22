bbb1d1137304f0696cf84bf9e8643b3bf733d948
Roman Strobl <roman.strobl@wultra.com>
2020-05-22 14:21:47 +0200

    Update version to 0.25.0-SNAPSHOT Fix #230: Improve logic for unknown signature type

--- powerauth-restful-security-javaee/src/main/java/io/getlime/security/powerauth/rest/api/jaxrs/provider/PowerAuthAuthenticationProvider.java
+++ powerauth-restful-security-javaee/src/main/java/io/getlime/security/powerauth/rest/api/jaxrs/provider/PowerAuthAuthenticationProvider.java
@@ -216,7 +216,7 @@ public class PowerAuthAuthenticationProvider extends PowerAuthAuthenticationProv
 
         // Check if the signature type is allowed
         PowerAuthSignatureTypes expectedSignatureType = PowerAuthSignatureTypes.getEnumFromString(header.getSignatureType());
-        if (!allowedSignatureTypes.contains(expectedSignatureType)) {
+        if (expectedSignatureType == null || !allowedSignatureTypes.contains(expectedSignatureType)) {
             throw new PowerAuthAuthenticationException("POWER_AUTH_SIGNATURE_TYPE_INVALID");
         }
 
@@ -293,7 +293,7 @@ public class PowerAuthAuthenticationProvider extends PowerAuthAuthenticationProv
 
         // Check if the signature type is allowed
         PowerAuthSignatureTypes expectedSignatureType = auth.getSignatureFactors();
-        if (!allowedSignatureTypes.contains(expectedSignatureType)) {
+        if (expectedSignatureType == null || !allowedSignatureTypes.contains(expectedSignatureType)) {
             throw new PowerAuthAuthenticationException("POWER_AUTH_TOKEN_SIGNATURE_TYPE_INVALID");
         }
 
