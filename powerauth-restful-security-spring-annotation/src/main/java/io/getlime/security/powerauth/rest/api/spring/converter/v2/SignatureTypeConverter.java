b0cf7a9541e2a49e98180704861639dd8f8fb1e9
Roman Strobl <roman.strobl@wultra.com>
2020-07-22 14:42:40 +0200

    Add code comments

--- powerauth-restful-security-spring-annotation/src/main/java/io/getlime/security/powerauth/rest/api/spring/converter/v2/SignatureTypeConverter.java
+++ powerauth-restful-security-spring-annotation/src/main/java/io/getlime/security/powerauth/rest/api/spring/converter/v2/SignatureTypeConverter.java
@@ -53,6 +53,7 @@ public class SignatureTypeConverter {
         } catch (IllegalArgumentException ex) {
             logger.warn("Invalid signature type, error: {}", ex.getMessage());
             logger.debug("Error details", ex);
+            // Return null value which represents an unknown signature type
             return null;
         }
 
