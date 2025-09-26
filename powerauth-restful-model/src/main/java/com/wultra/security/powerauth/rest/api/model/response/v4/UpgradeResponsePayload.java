33699b720927b5a6708348044882b3fd94a1d4a1
Roman Strobl <roman.strobl@wultra.com>
2025-09-26 15:44:52 +0800

    Fix #579: Implement upgrade of protocol to crypto4

--- powerauth-restful-model/src/main/java/com/wultra/security/powerauth/rest/api/model/response/v4/UpgradeResponsePayload.java
+++ powerauth-restful-model/src/main/java/com/wultra/security/powerauth/rest/api/model/response/v4/UpgradeResponsePayload.java
@@ -2,7 +2,7 @@
  * PowerAuth integration libraries for RESTful API applications, examples and
  * related software components
  *
- * Copyright (C) 2018 Wultra s.r.o.
+ * Copyright (C) 2025 Wultra s.r.o.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
@@ -17,12 +17,12 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.wultra.security.powerauth.rest.api.model.response;
+package com.wultra.security.powerauth.rest.api.model.response.v4;
 
 import lombok.Data;
 
 /**
- * Response object for upgrade payload.
+ * Response object for upgrade start payload.
  *
  * @author Roman Strobl, roman.strobl@wultra.com
  *
@@ -30,9 +30,8 @@ import lombok.Data;
 @Data
 public class UpgradeResponsePayload {
 
-    /**
-     * Counter data.
-     */
+    private SharedSecretResponse sharedSecretResponse;
+    private ServerPublicKeys serverPublicKeys;
     private String ctrData;
 
 }
