07b2d4b834d15991222eb55a82da62adf4b9f910
Roman Štrobl <roman.strobl@wultra.com>
2025-11-19 19:11:14 +0100

    Fix #651: Update model for Shared Secret request and response (#652)

--- powerauth-restful-model/src/main/java/com/wultra/security/powerauth/rest/api/model/response/v4/SharedSecretResponse.java
+++ powerauth-restful-model/src/main/java/com/wultra/security/powerauth/rest/api/model/response/v4/SharedSecretResponse.java
@@ -21,6 +21,8 @@ package com.wultra.security.powerauth.rest.api.model.response.v4;
 
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * Shared secret response.
  *
@@ -29,7 +31,7 @@ import lombok.Data;
 @Data
 public class SharedSecretResponse {
 
-    private String ecdhe;
-    private String mlkem;
+    private String salt;
+    private List<String> encapsulatedKeys;
 
 }
