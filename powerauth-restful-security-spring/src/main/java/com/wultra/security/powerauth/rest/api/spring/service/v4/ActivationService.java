522d49d018d19d957e8ee1d8c1f934b1c632da2e
Roman Štrobl <roman.strobl@wultra.com>
2026-06-11 11:46:14 +0800

    Fix #715: Return unblock timestamp for activations (#716)

--- powerauth-restful-security-spring/src/main/java/com/wultra/security/powerauth/rest/api/spring/service/v4/ActivationService.java
+++ powerauth-restful-security-spring/src/main/java/com/wultra/security/powerauth/rest/api/spring/service/v4/ActivationService.java
@@ -441,6 +441,9 @@ public class ActivationService {
             );
             final ActivationStatusResponse response = new ActivationStatusResponse();
             response.setActivationStatus(paResponse.getStatusBlob());
+
+            final Date timestampBlockExpireDate = paResponse.getTimestampBlockExpire();
+            response.setTimestampBlockExpire(timestampBlockExpireDate != null ? timestampBlockExpireDate.getTime() : null);
             if (applicationConfiguration != null) {
                 final ActivationContext activationContext = activationContextConverter.fromActivationDetailResponse(paResponse);
                 response.setCustomObject(applicationConfiguration.statusServiceCustomObject(activationContext));
